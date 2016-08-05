0a4f03d6876db564a7795bbeb46416b5b7f87ca6
Dave Lasley <dave@laslabs.com>
2016-08-04 19:29:01 -0700

    [ADD] web_app_drawer: Create new module * Move apps menu to an app drawer * Move sidebar to top menu

---
+++ web_app_drawer/static/src/js/web_app_drawer.js
@@ -0,0 +1,49 @@
+/* Copyright 2016 LasLabs Inc.
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
+
+odoo.define('web_app_drawer', function(require) {
+    'use strict';
+    
+    var $ = require('$');
+    var base = require('web_editor.base');
+    var Menu = require('web.Menu');
+    
+    Menu.include({
+        
+        start: function() {
+            this.is_bound.done(function() {
+                
+            });
+            return this._super();
+        },
+        
+        reflow: function() {
+            this._super('all_outside');
+        },
+        
+    });
+    
+    base.ready().done(function () {
+        
+        var slideout = new Slideout({
+            'panel': document.getElementById('odooMenuBar'),
+            'menu': document.getElementById('odooAppDrawer'),
+            'padding': 500,
+            'tolerance': 70,
+        });
+        
+        $('.js_toggle_app_drawer').click(function() {
+            slideout.toggle();
+            var $clickZones = $('.openerp_webclient_container, ' +
+                                'a.oe_menu_leaf, ' +
+                                'a.oe_menu_toggler'
+                                );
+            $clickZones.click(function() {
+                slideout.toggle();
+                $clickZones.off('click');
+            });
+        });
+        
+    });
+    
+});
