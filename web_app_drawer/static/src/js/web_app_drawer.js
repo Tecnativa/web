ba1953b0da5f712bb069f349fde13f0495e25bff
Dave Lasley <dave@laslabs.com>
2016-08-12 20:00:26 -0700

    Switch SlideoutJS for Drawer

--- web_app_drawer/static/src/js/web_app_drawer.js
+++ web_app_drawer/static/src/js/web_app_drawer.js
@@ -1,19 +1,19 @@
 /* Copyright 2016 LasLabs Inc.
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
 
-odoo.define('web_app_drawer', function(require) {
+odoo.define('web_app_drawer.app_drawer', function(require) {
     'use strict';
     
     var $ = require('$');
-    var base = require('web_editor.base');
     var Menu = require('web.Menu');
+    var core = require('web.core');
     
     Menu.include({
         
         start: function() {
-            this.is_bound.done(function() {
-                
-            });
+            //this.is_bound.done(function() {
+            //    $('.drawer').drawer();
+            //});
             return this._super();
         },
         
@@ -23,27 +23,15 @@ odoo.define('web_app_drawer', function(require) {
         
     });
     
-    base.ready().done(function () {
-        
-        var slideout = new Slideout({
-            'panel': document.getElementById('odooMenuBar'),
-            'menu': document.getElementById('odooAppDrawer'),
-            'padding': 350,
-            'tolerance': 70,
+    core.bus.on('web_client_ready', null, function () {
+        $('.drawer').drawer();
+        var $clickZones = $('.openerp_webclient_container, ' +
+                             'a.oe_menu_leaf, ' +
+                             'a.oe_menu_toggler'
+                             );
+        $clickZones.click(function() {
+            $('.drawer').drawer('close');
         });
-        
-        $('.js-toggle-app-drawer').click(function() {
-            slideout.toggle();
-            var $clickZones = $('.openerp_webclient_container, ' +
-                                'a.oe_menu_leaf, ' +
-                                'a.oe_menu_toggler'
-                                );
-            $clickZones.click(function() {
-                slideout.toggle();
-                $clickZones.off('click');
-            });
-        });
-        
     });
-    
+            
 });
