bc2b561d49da9cbd4886352e01b4e3ed743c772a
Dave Lasley <dave@laslabs.com>
2016-08-17 11:17:00 -0700

    No focus on mobile

--- web_app_drawer/static/src/js/web_app_drawer.js
+++ web_app_drawer/static/src/js/web_app_drawer.js
@@ -6,12 +6,38 @@ odoo.define('web_app_drawer.app_drawer', function(require) {
     
     var $ = require('$');
     var Menu = require('web.Menu');
+    var SearchView = require('web.SearchView');
     var core = require('web.core');
     
     Menu.include({
         
         reflow: function() {
             this._super('all_outside');
+        },
+        
+    });
+    
+    SearchView.include({
+        
+        // ReImplement core toggle_visibility to not focus when mobile
+        toggle_visibility: function (is_visible) {
+            this.do_toggle(!this.headless && is_visible);
+            if (this.$buttons) {
+                this.$buttons.toggle(!this.headless && is_visible && this.visible_filters);
+            }
+            if (!this.headless && is_visible && !this.isMobile()) {
+                this.$('div.oe_searchview_input').last().focus();
+            }
+        },
+        
+        // For lack of Modernizr, TouchEvent will do
+        isMobile: function () {
+            try{
+                document.createEvent('TouchEvent');
+                return true;
+            } catch (ex) {
+                return false;
+            }
         },
         
     });
