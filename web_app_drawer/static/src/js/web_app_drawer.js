35d0f4813e0d433ac4b9f802e203f1b581e0f938
Dave Lasley <dave@laslabs.com>
2016-08-22 11:09:36 -0700

    * Restructure for modularity * Use accesskey for drawer toggle * Remove search widget * Add arrow key bindings

--- web_app_drawer/static/src/js/web_app_drawer.js
+++ web_app_drawer/static/src/js/web_app_drawer.js
@@ -7,7 +7,6 @@ odoo.define('web_app_drawer', function(require) {
     var $ = require('$');
     var Menu = require('web.Menu');
     var Class = require('web.Class');
-    var Widget = require('web.Widget');
     var SearchView = require('web.SearchView');
     var core = require('web.core');
     
@@ -22,14 +21,16 @@ odoo.define('web_app_drawer', function(require) {
         
         // Prevent focus of search field on mobile devices
         toggle_visibility: function (is_visible) {
-            var self = this,
-                $searchField = $('div.oe_searchview_input').last();
-            $searchField.one('focus', function(){
-                if (self.isMobile()) {
-                    event.preventDefault();
-                }
-            });
+             $('div.oe_searchview_input').last()
+                .one('focus', $.proxy(this.preventMobileFocus, this));
             return this._super(is_visible);
+        },
+        
+        // It prevents focusing of search el on mobile
+        preventMobileFocus: function(event) {
+            if (this.isMobile()) {
+                event.preventDefault();
+            }
         },
         
         // For lack of Modernizr, TouchEvent will do
@@ -47,90 +48,108 @@ odoo.define('web_app_drawer', function(require) {
     var AppDrawer = Class.extend({
         
         // Constants used as math modifiers for x and y axis
-        LEFT: -1,
-        RIGHT: 1,
-        UP: 1,
-        DOWN: -1,
-        NONE: 0,
-        KEY_MAP: {
-            37: LEFT,  // Left arrow
-            39: RIGHT,  // Right arrow
-            38: UP,  // Up arrow
-            33: UP,  // page up
-            40: DOWN,  // Down arrow
-            34:DOWN,  // page down
-        },
+        LEFT: [-1, 0],
+        RIGHT: [1, 0],
+        UP: [0, 1],
+        DOWN: [0, -1],
+        
+        isOpen: false,
+        $selectedAppLink: $(false),
+        currentPos: [0, 0],
         
         init: function() {
             this.$el = $('.drawer');
-            this.$searchField = new AppSearch(this);
-            this.$searchField.appendTo($('.css-app-drawer-search-container'));
+            this.$el.drawer();
+            this.directionCodes = {
+                37: this.LEFT,  // Left arrow
+                39: this.RIGHT,  // Right arrow
+                38: this.UP,  // Up arrow
+                33: this.UP,  // page up
+                40: this.DOWN,  // Down arrow
+                34: this.DOWN,  // page down
+            };
             var $clickZones = $('.openerp_webclient_container, ' +
                                 'a.oe_menu_leaf, ' +
                                 'a.oe_menu_toggler'
-                                ),
-                self = this,
-                keyMod = 91,
-                keyA = 65;
-            this.$el.drawer();
-            if (navigator.appVersion.indexOf('Mac') != -1) {
-                keyMod = 17;
-            }
-            var keyMap = {keyA:false, keyCode: false};
-            $(window)
-                .keydown(function(e) {
-                    keyMap[e.keyCode] = true;
-                    if (keyMap[keyA] && keyMap[keyMod]) {
-                        self.$el.drawer('toggle');
-                    }
-                })
-                .keyup(function(e) {
-                    if (e.keyCode in keyMap) {
-                        keyMap[e.keyCode] = false;
-                    }
-                })
-                .keypress(function(e) {
-                    if (KEY_MAP.keys().indexOf(e.keyCode) !== -1) {
-                        self.findAdjacentAppLink();
-                    }
-                });
-            $clickZones.click(function() {
-                self.$el.drawer('close');
-                $('.oe_secondary_menus_container')
-                    .parent()
-                    .collapse('hide');
-            });
-            this.$el.on('drawer.opened', function() {
-                self.onDrawerOpen();
-            });
+                                );
+            $clickZones.click($.proxy(this.handleClickZones, this));
+            this.$el.one('drawer.opened', $.proxy(this.onDrawerOpen, this));
+            // Core bus keypress doesn't seem to send arrow keys
+            core.bus.on('keydown', this, this.handleNavKeys);
         },
         
-        /* Generate x and y coords for app icons
-         * @return Deferred
-         */
+        // It provides handlers to hide drawer when "unfocused"
+        handleClickZones: function() {
+            this.$el.drawer('close');
+            $('.oe_secondary_menus_container')
+                .parent()
+                .collapse('hide');
+        },
+        
+        // It provides keyboard shortcuts for app drawer nav
+        handleNavKeys: function(e) {
+            if (!this.isOpen){
+                return;
+            }
+            if (Object.keys(this.directionCodes).indexOf(e.keyCode.toString()) !== -1) {
+                var $link = this.findAdjacentAppLink(
+                    this.$selectedAppLink,
+                    this.directionCodes[e.keyCode][0],
+                    this.directionCodes[e.keyCode][1]
+                );
+                this.selectAppLink($link);
+            }
+        },
+        
+        onDrawerClose: function() {
+            this.isOpen = false;
+            this.$selectedAppLink = $(false);
+            core.bus.trigger('drawer.closed');
+            this.$el.one('drawer.opened', $.proxy(this.onDrawerOpen, this));
+        },
+        
+        // Generate x and y coords for app icons
         onDrawerOpen: function() {
+            var self = this;
+            this.isOpen = true;
+            this.$appLinks = $('.css-app-drawer-icon-app').parent();
+            this.selectAppLink($(this.$appLinks[0]));
+            this.linkMap = {};  // Key by row, col
             var x = 0,
-                y = 0,
+                y = -1,
                 lastTop = -9999,
                 newTop = null,
-                $lastVal = null,
-                $appIcons = $('.css-app-drawer-icon-app');
-            _.each($appIcons, function(val) {
-                $lastVal = $(val.parentElement);
+                $lastVal = null;
+            _.each(this.$appLinks, function(val) {
+                $lastVal = $(val);
                 newTop = $lastVal.offset().top;
                 if (lastTop < newTop) {
                     y ++;
                     x = 0;
+                    self.linkMap[y] = {};
                 }
-                // Attrs were not showing up on el using .data()
-                $lastVal.attr('data-pos-x', x).attr('data-pos-y', y);
+                self.linkMap[y][x] = $lastVal;
+                $lastVal.data('pos-x', x).data('pos-y', y);
                 x ++;
                 lastTop = newTop;
             });
-            $appIcons.on('mouseenter', function() {
-                $appIcons.removeClass('keyboard-selected');
-            });
-            return $.Deferred();
+            this.$appLinks.on('mouseenter', $.proxy(this.selectAppLinkEvent, this));
+            this.$el.one('drawer.closed', $.proxy(this.onDrawerClose, this));
+            core.bus.trigger('drawer.opened');
+        },
+        
+        // It provides a proxy method allowing for app link select w/ events
+        selectAppLinkEvent: function(event) {
+            this.selectAppLink($(event.target));
+        },
+        
+        // It selects an app link visibly
+        selectAppLink: function($appLink) {
+            if ($appLink) {
+                this.$selectedAppLink = $appLink;
+                this.$appLinks.removeClass('keyboard-selected');
+                $appLink.addClass('keyboard-selected').focus();
+            }
         },
         
         /* Find icon adjacent to app link
@@ -140,17 +159,14 @@ odoo.define('web_app_drawer', function(require) {
          * @return jQuery match for adjacent applink (or empty)
          */
         findAdjacentAppLink: function($appLink, x_dir, y_dir) {
-            var x = $appLink.data(posX) + x_dir,
-                y = $appLink.data(posY) + y_dir;
-            return $('.css-app-drawer-icon-app[data-pos-x="' + x + '"][data-pos-y="' + y + '"]');
+            var x = parseInt($appLink.data('posX')) + x_dir,
+                y = parseInt($appLink.data('posY')) + y_dir;
+            try{
+                return this.linkMap[y][x];
+            } catch(ex) {
+                return;
+            }
         },
-        
-    });
-    
-    var AppSearch = Widget.extend({
-        
-        template: 'web_app_drawer.SearchField',
-        
         
     });
     
@@ -160,7 +176,6 @@ odoo.define('web_app_drawer', function(require) {
     
     return {
         AppDrawer: AppDrawer,
-        AppSearch: AppSearch,
     };
     
 });
